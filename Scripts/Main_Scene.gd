d4c18496dddd56ac446342a89b5d93f008dbad7d
JaziriGaming <jaziri@jazirigaming.com>
2024-06-03 04:32:55 -0500

    Added MainScene script
    Added the main_scene script and setup overlay animations

---
+++ Scripts/Main_Scene.gd
@@ -0,0 +1,26 @@
+extends Control
+class_name SceneManager
+
+@onready var anim_player = $AnimationPlayer
+
+var next_scene
+var last_scene
+
+func _ready():
+	EventBus.changing_scenes.connect(change_scene)
+	
+func change_scene(from: String, to_scene_name: String):
+	last_scene = get_node(from)
+	
+	next_scene = load("res://Scenes/" + to_scene_name + ".tscn").instantiate()
+	next_scene.modulate.a = 0
+	add_child(next_scene)
+	anim_player.play("fade_in_overlay")
+	transfer_data()
+	next_scene.setup_scene()
+	
+func transfer_data():
+	if "scene_parameters" in last_scene:
+		for i in last_scene.scene_parameters:
+			if i in next_scene.scene_parameters:
+				next_scene.scene_parameters[i] = last_scene.scene_parameters[i]
